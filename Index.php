27a299b87e1fa8b827b183684fe29843f6c45a84
FernandaM28 <149074737+FernandaM28@users.noreply.github.com>
2023-10-31 10:06:57 -0500

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=n v, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1>Este es mi proyecto Github</h1> 
+    <img src="img/venecia.jpg" alt="50">
+</body>
+</html>
